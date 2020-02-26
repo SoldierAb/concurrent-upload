82d54776bc641b68e51fbefdc2d27d32b663d3a9
chengj <ab140140@163.com>
2020-02-26 13:44:48 +0800

    feat: create concurrent util

--- web/src/FileContainer.jsx
+++ web/src/FileContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { uploadFetch } from './utils/upload';
+import ConcurrentUtil from './utils/concurrent'
 import "./file.css";
 
 export default class FileContainer extends React.Component {
     constructor() {
         super();
+        this.concurrent = new ConcurrentUtil(this.uploadRequest,this.mergeRequest);
         this.state = {
             file: null,
             fileSplit: [],
@@ -17,132 +19,172 @@ export default class FileContainer extends React.Component {
         this.setState({
             file
         });
-        event.preventDefault();
-        this.splitZip(file);
-    }
-
-    handleSubmit = (event) => {
-        let startTime = new Date().getTime();
-        console.log("start at: -----> ", startTime);
-        this.handleUpload().then((res) => {
-            let endTime = new Date().getTime();
-            console.log("<------ end at: -----> ", endTime, "   duration:   ", endTime - startTime);
-            alert(res);
+        this.concurrent.setState({
+            file
         });
         event.preventDefault();
     }
 
-    handleUpload = (limitCount = 4) => new Promise(async (resolve) => {
-        let { fileSplit, file } = this.state,
-            { name: file_name } = file,
-            fileSequenceItem = [],
-            fileSequence = [],
-            fileMark = "";
-
-        //1.请求服务端，查找对应文件块是否存在
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const startTime = new Date().getTime(),
+            { name: file_name } = this.state.file;
+        console.log("start at: -----> ", startTime);
+        //1.请求服务端，查找对应文件块
         const { data: { list: pathList } } = await uploadFetch({
             action: '/v1/resources/blobs',
             data: {
                 file_name
             }
         })
-        //文件过滤
-        fileSplit = fileSplit.map((blob, index) => {
-            fileMark += `${index}`
-            if ((Array.isArray(pathList)&&pathList.length&&!pathList.find(pp => pp.split("_index_")[1] === `${index}`)) || !pathList) {
-                return { file: blob, index: `${fileMark}_index_${index}` }
-            } else {
-                return null
-            }
-        })
-        fileSplit = fileSplit.filter(item => item);
-
-        //2.上传缺失的文件块
-
-        console.log(fileSplit);
-
-        if (Array.isArray(fileSplit)&&fileSplit.length===0){
-            return uploadFetch({
-                action: '/v1/resources/merge',
-                data: {
-                    file_name
-                },
-            }).then(() => {
-                resolve("success");
-            })
-        }
-
-        let fileSequenceLen = fileSplit.length;
-
-        fileSplit.forEach(file => {
-            fileSequenceItem.push(file);
-            fileSequenceLen -= 1;
-            if (fileSequenceItem.length === limitCount) {
-                fileSequence.push(fileSequenceItem);
-                fileSequenceItem = [];
-            }
-            if (fileSequenceLen === 0 && fileSequenceItem.length) {
-                fileSequence.push(fileSequenceItem);
-            }
-        })
-
-
-        fileSequence.forEach(async (fileArray, index) => {
-            await this.sequenceUpload(fileArray, file_name);
-            if (index === fileSequence.length - 1) {
-                //结束传输，发送合并请求
-                uploadFetch({
-                    action: '/v1/resources/merge',
-                    data: {
-                        file_name
-                    },
-                }).then(() => {
-                    resolve("success");
-                })
-            }
-        })
-
-    })
-
-    sequenceUpload = (fileSequenceItem, file_name) => {
-        return Promise.all([
-            ...fileSequenceItem.map(item => {
-                return uploadFetch({
-                    action: '/v1/resources/upload',
-                    file: item.file,
-                    data: {
-                        file_name,
-                        index: item.index,
-                    }
-                })
-            })
-        ])
+        this.concurrent.handleUpload(pathList).then((res) => {
+            const endTime = new Date().getTime();
+            console.log("<------ end at: -----> ", endTime, "   duration:   ", endTime - startTime);
+            alert('= = ',res);
+        });
     }
 
     /** 
-     * @param {number} 默认 1M
-     * @returns {blob array}
+     * @description 文件上传
     */
-    splitZip(file, size = 1024 * 1024 * 1) {
-        let fileSplit = [];
-
-        if (file.size > size) {
-            let start_index = 0, end_index = 0;
-            while (true) {
-                end_index += size;
-                const blob = file.slice(start_index, end_index);
-                start_index += size;
-                if (!blob.size) break;
-                fileSplit.push(blob);
+    uploadRequest=({file,file_name,file_mark:index})=>{
+       return uploadFetch({
+            action: '/v1/resources/upload',
+            file,
+            data: {
+                file_name,
+                index,
             }
-        } else {
-            fileSplit.push(file);
-        }
-        this.setState({
-            fileSplit
-        });
-        return fileSplit;
+        })
     }
+
+    /** 
+     * @description 合并请求
+    */
+    mergeRequest=({file_name})=>{
+        return uploadFetch({
+            action: '/v1/resources/merge',
+            data: {
+                file_name
+            },
+        })
+    }
+
+    // /** 
+    //  * @description 上传任务
+    // */
+    // handleUpload = (limitCount = 4) => new Promise(async (resolve) => {
+    //     let { fileSplit, file } = this.state,
+    //         { name: file_name } = file,
+    //         fileSequenceItem = [],
+    //         fileSequence = [],
+    //         fileMark = "";
+
+    //     //1.请求服务端，查找对应文件块是否存在
+    //     const { data: { list: pathList } } = await uploadFetch({
+    //         action: '/v1/resources/blobs',
+    //         data: {
+    //             file_name
+    //         }
+    //     })
+    //     //文件过滤
+    //     fileSplit = fileSplit.map((blob, index) => {
+    //         fileMark += `${index}` //文件分片标识
+    //         if ((Array.isArray(pathList)&&pathList.length&&!pathList.find(pp => pp.split("_index_")[1] === `${index}`)) || !pathList) {
+    //             return { file: blob, index: `${fileMark}_index_${index}` }
+    //         } else {
+    //             return null
+    //         }
+    //     })
+    //     fileSplit = fileSplit.filter(item => item);
+
+    //     //2.上传缺失的文件块
+    //     if (Array.isArray(fileSplit)&&fileSplit.length===0){
+    //         return uploadFetch({
+    //             action: '/v1/resources/merge',
+    //             data: {
+    //                 file_name
+    //             },
+    //         }).then(() => {
+    //             resolve("success");
+    //         })
+    //     }
+
+    //     let fileSequenceLen = fileSplit.length;
+
+    //     fileSplit.forEach(file => {
+    //         fileSequenceItem.push(file);
+    //         fileSequenceLen -= 1;
+    //         if (fileSequenceItem.length === limitCount) {
+    //             fileSequence.push(fileSequenceItem);
+    //             fileSequenceItem = [];
+    //         }
+    //         if (fileSequenceLen === 0 && fileSequenceItem.length) {
+    //             fileSequence.push(fileSequenceItem);
+    //         }
+    //     })
+
+    //     //分片队列上传操作
+    //     fileSequence.forEach(async (fileArray, index) => {
+    //         await this.sequenceUpload(fileArray, file_name);
+    //         if (index === fileSequence.length - 1) {
+    //             //结束传输，发送合并请求
+    //             uploadFetch({
+    //                 action: '/v1/resources/merge',
+    //                 data: {
+    //                     file_name
+    //                 },
+    //             }).then(() => {
+    //                 resolve("success");
+    //             })
+    //         }
+    //     })
+
+    // })
+
+    // /** 
+    //  * @description 分片队列上传
+    // */
+    // sequenceUpload = (fileSequenceItem, file_name) => {
+    //     return Promise.all([
+    //         ...fileSequenceItem.map(item => {
+    //             return uploadFetch({
+    //                 action: '/v1/resources/upload',
+    //                 file: item.file,
+    //                 data: {
+    //                     file_name,
+    //                     index: item.index,
+    //                 }
+    //             })
+    //         })
+    //     ])
+    // }
+
+    // /** 
+    //  * @description 文件分片
+    //  * @param {number} 默认 1M
+    //  * @returns {blob array}
+    // */
+    // splitZip(file, size = 1024 * 1024 * 1) {
+    //     let fileSplit = [];
+
+    //     if (file.size > size) {
+    //         let start_index = 0, end_index = 0;
+    //         while (true) {
+    //             end_index += size;
+    //             const blob = file.slice(start_index, end_index);
+    //             start_index += size;
+    //             if (!blob.size) break;
+    //             fileSplit.push(blob);
+    //         }
+    //     } else {
+    //         fileSplit.push(file);
+    //     }
+    //     this.setState({
+    //         fileSplit
+    //     });
+    //     return fileSplit;
+    // }
 
     render() {
         return (
---
+++ web/src/utils/concurrent.js
@@ -0,0 +1,132 @@
+
+
+export default class Concurrent {
+    constructor(
+        upload,
+        merge,
+        size = 1024 * 1024 * 1,  //默认 1M
+        limit = 4, //并发限制 默认 4
+    ) {
+        this.state = {
+            upload,
+            merge,
+            size,
+            limit,
+            file: null,    //当前文件对象
+            fileSplit: [],   // 文件分片
+        }
+    }
+
+    /** 
+     * @description state 更新
+    */
+    setState(obj) {
+        this.state = { ...this.state, ...obj };
+    }
+
+    /** 
+     * @description 文件分片
+     * @param {number} 默认 1M
+     * @returns {blob array}
+    */
+    splitZip() {
+        const { file, size } = this.state;
+        const fileSplit = [];
+
+        if (file.size > size) {
+            let start_index = 0, end_index = 0;
+            while (true) {
+                end_index += size;
+                const blob = file.slice(start_index, end_index);
+                start_index += size;
+                if (!blob.size) break;
+                fileSplit.push(blob);
+            }
+        } else {
+            fileSplit.push(file);
+        }
+        this.setState({
+            fileSplit
+        });
+        return fileSplit;
+    }
+
+    /** 
+    * @description 上传行为；如果 pathList 为  falsy或[] 则上传当前所有分片
+   */
+    handleUpload = async (pathList) => {
+
+        this.splitZip();
+
+        let { fileSplit, file, limit } = this.state,
+            { name: file_name } = file,
+            fileSequenceItem = [],
+            fileSequence = [],
+            fileMark = "";
+
+        //文件过滤
+        fileSplit = fileSplit.map((blob, index) => {
+            fileMark += `${index}` //文件分片标识
+            if ((Array.isArray(pathList) && pathList.length && !pathList.find(pp => pp.split("_index_")[1] === `${index}`)) || !pathList.length) {
+                return { file: blob, index: `${fileMark}_index_${index}` }
+            } else {
+                return null
+            }
+        })
+        fileSplit = fileSplit.filter(item => item);
+
+        // 没有缺失的片段 ，发送合并请求
+        if (Array.isArray(fileSplit) && fileSplit.length === 0) {
+            return this.state.merge.call(this, { file_name })
+        }
+
+        //2.上传缺失的文件块
+        let fileSequenceLen = fileSplit.length;
+
+        fileSplit.forEach(file => {
+            fileSequenceItem.push(file);
+            fileSequenceLen -= 1;
+            if (fileSequenceItem.length === limit) {
+                fileSequence.push(fileSequenceItem);
+                fileSequenceItem = [];
+            }
+            if (fileSequenceLen === 0 && fileSequenceItem.length) {
+                fileSequence.push(fileSequenceItem);
+            }
+        })
+
+        await this.fileSequenceUpload(fileSequence, file_name)
+        return this.state.merge.call(this, { file_name })
+    }
+
+    /** 
+     * @description 文件分片队列上传
+    */
+    fileSequenceUpload = async (fileSequence, file_name) => {
+        let sequenceLen=fileSequence.length;
+        while (sequenceLen) {
+            const fileSequenceItem = fileSequence.shift();
+            await this.sequenceUpload(fileSequenceItem, file_name)
+            sequenceLen-=1;
+        }
+        return true
+    }
+
+
+    /** 
+     * @description 分片上传
+    */
+    sequenceUpload = (fileSequenceItem, file_name) => {
+        return Promise.all(
+            fileSequenceItem.map(item =>
+                this.state.upload.call(this, {
+                    file: item.file,
+                    file_mark: item.index,
+                    file_name,
+                })
+            )
+        )
+    }
+
+
+}
